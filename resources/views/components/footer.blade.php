0792d80bbb7e26952540c1e33911d2cafa35e619
unknown <rdiansyah.muhammad@gmail.com>
2023-12-07 16:29:41 +0700

    Feature Soal

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+    <div class="center">
+
+        Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="#">CBT Developer </a>
+    </div>
+
+
+
+
+
+</footer>
